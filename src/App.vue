4ed17ec70f6526919845d54163ac36feaa686825
AswinchristoJ <azooschristo@gmail.com>
2020-09-19 17:13:45 +0900

    updated vuetify, அடிக்குறிப்பு சேர்ப்பு

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
+<!-- அடிக்குறிப்பு -->
 <template>
   <div>
     <v-main transition="slide-x-transition">
       <router-view></router-view>
     </v-main>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Footer from "./views/Footer";
+export default {
+  components: {
+    Footer
+  }
+};
+</script>
---
+++ src/views/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <p>அஸ்வின் கிறிஸ்டோ / Aswin Christo</p>
+    <p>தொடர்புக்கு: azooschristo@gmail.com</p>
+  </div>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 80px;
+  background: #0000007d;
+  color: white;
+  text-align: right;
+  padding: 15px;
+  line-height: 1.6;
+  font-size: 0.8rem;
+}
+</style>
